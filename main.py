8e9d4b1fd43a0aa5c6c876859bea6ffb74ca12ab
fernandopaz1 <ferpaz9292@gmail.com>
2021-07-10 15:19:38 -0300

    feat: metodo put update_items

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from typing import Optional
+from pydantic import BaseModel
+from typing import Optional #para parametros opcionales
 
 app = FastAPI()
 
@@ -14,3 +15,16 @@ def read_item(item_id: int, q: Optional[str] = None):
 # montamos el servidor con uvicorn main:app --reload
 # la documentacion esta en http://127.0.0.1:8000/docs o alternativamente http://127.0.0.1:8000/redoc
 
+# Aca defino el tipo de objeto que recibe el metodo put
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+# update item debe recibir el numero que se lo paso por la url
+# y un objeto tipo Item que me lo pasan por el body
+# el body se lo podemos pasar desde la pagina de documentacion
+# en el boton try out del metodo put
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
